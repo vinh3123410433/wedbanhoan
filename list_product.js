b2e375a13d8efa167f85f0ec7fc48855b0fe4a2c
CNhunq <phamn5265@gmail.com>
2024-11-05 19:13:51 +0700

    Add files via upload

---
+++ list_product.js
@@ -0,0 +1,575 @@
+var list_products = [{
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"Whole Cake",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+},
+{
+    id: 2,
+    name: "Fresh Mango Cream Cake",
+    price: "350000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/01/FRESH-MANGO.png",
+    type:"Whole Cake",
+    description: "Bánh kem bông lan vani kem tươi và mứt trái cây",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 3,
+    name: "Black Forest Cake",
+    price: "340000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/01/BLACK-FOREST.png     ",
+    type:"Whole Cake",
+    description: "Bánh kem sô cô la cùng kem tươi và mứt cherry",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 4,
+    name: "Fresh Strawberry Yogurt Cream Cake",
+    price: "400000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/01/YOGURT.png",
+    type:"Whole Cake",
+    description: "Bông lan vani cùng kem sữa chua, mứt dâu và trái cây tươi",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 5,
+    name: "Strawberry Brownie Mousse Cake",
+    price: "420000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/01/STRAWBERRY-MOUSSE.png",
+    type:"Whole Cake",
+    description: "Bông lan sô cô la, kem mousse sô cô la và caramel",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 6,
+    name: "Fruity Butter Cream Cake",
+    price: "420000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/01/FRUITY.png",
+    type:"Whole Cake",
+    description: "Bông lan kem bơ custard cùng các loại trái cây tươi",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 7,
+    name: "Choco Cheese Mousse Cake",
+    price: "450000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/01/CHOCO-CHEESE.png",
+    type:"Whole Cake",
+    description: "Bánh kem sô cô la, mousse sô cô la và kem phô mai",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 8,
+    name: "Mango Coco Mousse Cake (Sliced)",
+    price: "400000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/05/Mango-Coconut-Mousse-Cake.png",
+    type:"Short Cake",
+    description: "Bánh kem mousse dừa, xoài & custard xoài chanh dây",
+    taste: "Chua, Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 9,
+    name: "Miroir Café Mousse Cake",
+    price: "80000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/01/cafe.png",
+    type:"Short Cake",
+    description: "Bông lan sô cô la, kem mousse cà phê và caramel",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 10,
+    name: "Miroir Au Chocolat Mousse Cake",
+    price: "880000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/01/chcoo.png",
+    type:"Short Cake",
+    description: "Bông lan sô cô la, kem mousse vani và sô cô la",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 11,
+    name: "Miroir Lime Mousse Cake",
+    price: "80000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/01/lime.png",
+    type:"Short Cake",
+    description: "Bông lan vani, kem mousse chanh và dâu tây",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 12,
+    name: "Fresh Strawberry Yogurt Cake (Short)",
+    price: "90000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2021/11/yogurt-sliced.png",
+    type:"Short Cake",
+    description: "Bông lan vani cùng kem sữa chua, mứt dâu và trái cây tươi",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 13,
+    name: "Tiramisu Cheesecake (Sliced)",
+    price: "120000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/01/tira-sliced.png",
+    type:"Short Cake",
+    description: "Bánh kem tiramisu cà phê và kem phô mai béo ngậy",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 14,
+    name: "Black Forest Cake (Sliced)",
+    price: "100000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/01/black-forest-sliced.png",
+    type:"Short Cake",
+    description: "Bánh kem sô cô la cùng kem tươi và mứt cherry",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 15,
+    name: "Brioche Bread",
+    price: "120000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/brioche.png",
+    type:"Bread and Pastry",
+    description: "Bánh mì hoa cúc thơm ngon phủ hạnh nhân",
+    taste: "Ngọt",
+    size: "22cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+     {
+    id: 16,
+    name: "Fresh Cream Loaf Bread",
+    price: "320000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/fresh-cream-loaf.png",
+    type:"Bread and Pastry",
+    description: "Bánh gối nhân kem sữa đặc ngọt dịu",
+    taste: "Ngọt",
+    size: "20cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+      {
+    id: 17,
+    name: "Best Ever Garlic Bread",
+    price: "140000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/garlic-butter.png",
+    type:"Bread and Pastry",
+    description: "Bánh mì bơ tỏi kem phô mai",
+    taste: "Mặn",
+    size: "12cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+       {
+    id: 18,
+    name: "Mini Butter Croissant (5Pcs)",
+    price: "35000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/mini-croissant.png",
+    type:"Bread and Pastry",
+    description: "Bánh Croissant Bơ ngàn lớp thơm ngon",
+    taste: "Ngọt",
+    size: "10cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+        {
+    id: 19,
+    name: "Mini Choco Pastry Twists (8Pcs)",
+    price: "40000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/chocochi.png",
+    type:"Bread and Pastry",
+    description: "Những chiếc bánh ngàn lớp xoắn sô cô la chip mini",
+    taste: "Ngọt",
+    size: "7cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+         {
+    id: 20,
+    name: "Korean Milk Cream Bread",
+    price: "20000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/korean-milke.png",
+    type:"Bread and Pastry",
+    description: "Bánh mì tươi cùng nhân kem sữa ngọt dịu",
+    taste: "Ngọt",
+    size: "12cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+          {
+    id: 21,
+    name: "Corn Custard Donut",
+    price: "30000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2024/09/corn-donut.png",
+    type:"Bread and Pastry",
+    description: "Bánh Donut kem custard Bắp và bắp tươi",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 22,
+    name: "Chocolate Macaron",
+    price: "10000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/choco-macaron-1.png",
+    type:"Dessert",
+    description: "Bánh Macaron giòn tan cùng kem tươi sô cô la",
+    taste: "Ngọt",
+    size: "5cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 23,
+    name: "Strawberry Macaron",
+    price: "10000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/bery-macaron-1.png",
+    type:"Dessert",
+    description: "Bánh Macaron giòn tan cùng kem tươi mứt dâu tây",
+    taste: "Ngọt",
+    size: "5cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 24,
+    name: "Yuzu Macaron",
+    price: "10000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/yuzu-macaron-1.png",
+    type:"Dessert",
+    description: "Bánh Macaron giòn tan cùng kem tươi thanh yên",
+    taste: "Ngọt",
+    size: "5cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 25,
+    name: "Vanilla Macaron",
+    price: "110000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/vani-macaron.png",
+    type:"Dessert",
+    description: "Bánh Macaron giòn tan cùng kem vani",
+    taste: "Ngọt",
+    size: "5cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 26,
+    name: "Mango Royal Pudding",
+    price: "20000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/mango-pudding.png",
+    type:"Dessert",
+    description: "Lớp Pudding mềm mại, mịn màng cùng mứt xoài chua ngọt",
+    taste: "Ngọt",
+    size: "200ml",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 27,
+    name: "Original Royal Pudding",
+    price: "20000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/original-pudding.png",
+    type:"Dessert",
+    description: "Lớp Pudding mịn màng vị vani cùng caramel",
+    taste: "Ngọt",
+    size: "200ml",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 28,
+    name: "Mr.Bear Madeleine",
+    price: "15000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/Mr.Bear_.png",
+    type:"Gifts",
+    description: "Bánh Madeleine mềm mại vị sô cô la hình chú Gấu",
+    taste: "Ngọt",
+    size: "15cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 29,
+    name: "Ms.Bear Madeleine",
+    price: "15000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/Ms.Bear_.png",
+    type:"Gifts",
+    description: "Bánh Madeleine mềm mại vị vani hình chú Gấu",
+    taste: "Ngọt",
+    size: "15cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 30,
+    name: "Premium Honey Castella",
+    price: "22000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/cSSTEALL.png",
+    type: "Gifts",
+    description: "Bông lan Castella mềm mịn vị mật ong",
+    taste: "Ngọt",
+    size: "10cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 31,
+    name: "Silky Roll Cake",
+    price: "30000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/SILKY.png",
+    type: "Gifts",
+    description: "Bông lan cuộn nhân kem vani và nho khô thượng hạng",
+    taste: "Ngọt",
+    size: "20cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 32,
+    name: "Choco Banana Roll Cake",
+    price: "30000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/CHOCO.png",
+    type: "Gifts",
+    description: "Bông lan sô cô la thượng hạng nhập khẩu Hàn Quốc",
+    taste: "Ngọt",
+    size: "20cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 33,
+    name: "Velvet Strawberry Roll Cake",
+    price: "30000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/VELVET.png",
+    type: "Gifts",
+    description: "Bánh bông lan dâu tây cuộn kem tươi dâu tây",
+    taste: "Ngọt",
+    size: "20cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 34,
+    name: "Caramel Walnut Pound Cake",
+    price: "50000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/WALNUT-POUND.png",
+    type: "Gifts",
+    description: "Bánh Pound Cake Caramel và Hạt Óc Chó",
+    taste: "Ngọt",
+    size: "22cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 35,
+    name: "Double Chocochip Cookie (Box)",
+    price: "60000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/double.png",
+    type:"Cookies",
+    description: "Cookie giòn tan cùng sô cô la đậm vị",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+     {
+    id: 36,
+    name: "English Toffee Cookie (Box)",
+    price: "60000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2023/02/cranberry-walnut-box.png",
+    type:"Cookies",
+    description: "Cookie giòn tan cùng kẹo bơ béo ngậy",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+      {
+    id: 337,
+    name: "Macadamia Cookie",
+    price: "7000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2023/02/macadamia-1.png",
+    type:"Cookies",
+    description: "Cookie giòn tan cùng hạt mắc ca bổ dưỡng",
+    taste: "Mặn",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+       {
+    id: 38,
+    name: "English Toffee Cookie",
+    price: "7000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/toffee.png",
+    type:"Cookies",
+    description: "Cookie giòn tan cùng kẹo bơ béo ngậy",
+    taste: "Ngọt",
+    size: "7cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+        {
+    id: 339,
+    name: "Double Chocochip Cookie",
+    price: "7000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/chocolate.png",
+    type:"Cookies",
+    description: "Cookie giòn tan cùng sô cô la đậm vị",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+         {
+    id: 40,
+    name: "Cranberry Walnut Cookie",
+    price: "7000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/cranberry-walnut.png",
+    type:"Cookies",
+    description: "Cookie giòn tan cùng hạt óc chó và nam việt quất",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+          {
+    id: 41,
+    name: "Walnut Chocolate Cookie",
+    price: "7000",
+    image: "https://parisbaguette.com.vn/wp-content/uploads/2022/07/choco-walnut.png",
+    type:"Cookies",
+    description: "Cookie sô cô la giòn tan cùng hạt óc chó thơm ngon",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+]
