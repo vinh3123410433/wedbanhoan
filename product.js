49d5b3842992281b941aae42fd274ec60ac95b21
vinh3123410433 <nguyenvinhthai11a8@gmail.com>
2024-11-05 17:50:07 +0700

    Create product.js

---
+++ product.js
@@ -0,0 +1,295 @@
+var list_products = [{
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"WHOLE CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+},
+{
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"WHOLE CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"WHOLE CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"WHOLE CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"WHOLE CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"WHOLE CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"WHOLE CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"SHORT CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"SHORT CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"SHORT CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"SHORT CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"SHORT CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"SHORT CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"SHORT CAKE",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"BEAD AND PASTRY",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+     {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"BEAD AND PASTRY",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+      {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"BEAD AND PASTRY",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+       {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"BEAD AND PASTRY",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+        {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"BEAD AND PASTRY",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+         {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"BEAD AND PASTRY",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+          {
+    id: 1,
+    name: "Corn Mousse Cake",
+    price: "320000",
+    image: "image/product/corn-mousse.png",
+    type:"BEAD AND PASTRY",
+    description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+]
